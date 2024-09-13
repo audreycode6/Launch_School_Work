8edba3e41963e5623e9b721a83d4d7c5c3c4c2a8
Audrey Theriault-Allaire <audrey2699@gmail.com>
2024-09-13 13:38:39 -0700

    file to test other LS student submissions

---
+++ PY110/lesson_3/test_ttt.py
@@ -0,0 +1,6 @@
+'''IDEAS:
+-let player know number options go 1-9 left to right top to bottom
+- let player pick x or o symbol
+- long lines     print(f"You are {player_mark['user']}"
+          "and Computer is {player_mark['computer']}.")
+-'''
