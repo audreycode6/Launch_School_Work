08d722a6b6a3f40361c78a509ce4786c5491b888
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-02-17 11:33:00 -0800

    updated twentyone program to play again by having a single_game logic and added function for match logic: prompt if playing again while player isnt broke or too rich

--- PY120/lesson_5/oop_twenty1.py
+++ PY120/lesson_5/oop_twenty1.py
@@ -20,9 +20,9 @@ class Deck:
                 card = random.choice(self.deck)
                 self.deck.remove(card)
                 cards_dealt.append(card)
-            except IndexError: 
+            except IndexError:
                 print("Ran out of cards in deck!")
-                #self.deck = self.reset() # TODO TEST
+                # TODO maybe unneccesary
 
         return cards_dealt
         # print(f"\nLEngth: {len(self.deck)}")
@@ -44,7 +44,7 @@ class Participant():
         new_card = current_deck.deal(1)
         self.hand.append(new_card[0])
         clear_screen()
-        print(f"{new_card[0]} was added!\n")
+        print(f"{new_card[0]} was added!")
 
     def display_hand(self, player="You have"):
         hand_size = len(self.hand)
@@ -93,13 +93,13 @@ class Player(Participant):
     def update_money(self, player_won):
         if player_won:
             self.betting_balance += 1
-            print("\nYou earned another dollar!")
+            print("You earned another dollar!")
         else:
             self.betting_balance -= 1
-            print("\nYou lost a dollar!")
+            print("You lost a dollar!")
 
     def display_money_available(self):
-        print(f"You currently have ${self.betting_balance} for betting.")
+        print(f"You have ${self.betting_balance} for betting.")
 
 class Dealer(Participant):
     def __init__(self):
@@ -118,7 +118,7 @@ class Dealer(Participant):
         return f"{player}: {one_card} and unknown card"
 
     def reveal_hand(self):
-        return super().display_hand("Dealer's hand")
+        return super().display_hand("Dealer's Hand")
 
     def deal(self):
         # STUB
@@ -129,16 +129,52 @@ class TwentyOneGame:
         self.deck = Deck()
         self.player = Player()
         self.dealer = Dealer()
+        self.playing = True
+        self.match_over = False
 
     def start(self):
         self.display_welcome_message()
+        while True:
+            self.play_single_game()
+            self.display_result_update_money()
+            self.match_over = self.is_match_over()
+            if not self.match_over:
+                self.playing = self.play_again()
+            if (not self.playing or
+                self.match_over):
+                break
+            self.reset_game()
+
+        if self.match_over:
+            self.display_match_results()
+        self.display_goodbye_message()
+
+    def play_single_game(self):
         self.player.display_money_available()
         self.deal_cards()
         self.player_turn()
         if not self.player.is_busted():
             self.dealer_turn()
-        self.display_result_update_money()
-        self.display_goodbye_message()
+
+    def is_match_over(self):
+        return self.player.betting_balance in [0, 10]
+
+    def reset_game(self):
+        '''reset player scores & hands, and deck + terminal'''
+        self.player.score = 0
+        self.dealer.score = 0
+        self.deck.reset()
+        self.player.hand.clear()
+        self.dealer.hand.clear()
+        clear_screen()
+
+    def display_match_results(self):
+        print("... GAME OVER ...\n")
+        if self.match_over:
+            if self.player.betting_balance == 10:
+                print("You're too good at this game! Take your money and go.")
+            else:
+                print("You're broke... No money, no playing!")
 
     def deal_cards(self):
         for card in self.deck.deal():
@@ -147,6 +183,7 @@ class TwentyOneGame:
             self.dealer.hand.append(card)
 
     def show_cards(self):
+        print()
         print(self.dealer.display_hidden_hand())
         print(self.player.display_hand())
 
@@ -154,7 +191,7 @@ class TwentyOneGame:
         prompt = "==> Would you like to hit or stay? (h/s): "
         while True:
             self.show_cards()
-            print(f"\nYou currently have {self.player.current_score()} points")
+            print(f"You currently have {self.player.current_score()} points\n")
 
             while True:
                 hit_or_stay = input(prompt).lower()
@@ -177,8 +214,9 @@ class TwentyOneGame:
         while self.dealer.current_score() < 17:
             self.dealer.hit(self.deck)
         if not self.dealer.is_busted():
-            self.dealer.stay() # TODO maybe unneccessary
+            self.dealer.stay()
         input("\n==> Press Enter to see the results: ")
+        clear_screen()
 
     def determine_winner(self):
         player_score = self.player.current_score()
@@ -203,7 +241,7 @@ class TwentyOneGame:
         self.player.update_money(player_won)
 
     def play_again(self):
-        prompt = "\n==> Want to continue playing? (y/n): "
+        prompt = "==> Want to continue playing? (y/n): "
         while True:
             playing_choice = input(prompt).lower()
             if playing_choice in ['y', 'n']:
@@ -217,24 +255,25 @@ class TwentyOneGame:
         self.display_rules()
 
     def display_rules(self):
-        print("... Rules go here ...")
+        print("... Rules go here ...\n")
 
     def display_goodbye_message(self):
-        print("\nThanks for playing Twenty One! Goodbye!")
+        print("Thanks for playing Twenty One! Goodbye!")
 
     def display_result_update_money(self):
         clear_screen()
         print("* RESULTS *")
         winner = self.determine_winner()
         if winner == "Tie":
-            print("It's a tie!\n")
+            print("It's a tie!")
         else:
-            print(f"{winner} won!\n")
-        print(f"{self.player.display_hand("Your hand")} \n"
-                f"Your score: {self.player.current_score()}\n")
-        print(f"{self.dealer.reveal_hand()} \n"
-                f"Dealers score: {self.dealer.current_score()}")
-        self.update_player_money(winner)
+            print(f"{winner} won!")
+            self.update_player_money(winner)
         self.player.display_money_available()
+        print(f"\n{self.dealer.reveal_hand()} \n"
+                f"Dealers Score: {self.dealer.current_score()}\n")
+        print(f"{self.player.display_hand("Your Hand")} \n"
+                f"Your Score: {self.player.current_score()}\n")
+
 game = TwentyOneGame()
 game.start()
