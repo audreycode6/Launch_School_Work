658cd92d4a9ca6386e2b15e04ae9fdbc46e3d68c
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-04-02 10:08:56 -0700

    Stop ignoring todo_class.py

---
+++ PY130/lesson_1/todo_class.py
@@ -0,0 +1,60 @@
+class Todo:
+    COMPLETE = "X"
+    INCOMPLETE = " "
+
+    def __init__(self, title):
+        self._title = title
+        self._done = False
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def done(self):
+        return self._done
+
+    @done.setter
+    def done(self, new_value):
+        self._done = new_value
+
+    def __str__(self):
+        symbol = Todo.COMPLETE if self.done else Todo.INCOMPLETE
+        return f"[{symbol}] {self._title}"
+
+    def __eq__(self, other):
+        if not isinstance(other, Todo):
+            return NotImplemented
+        return  self.title == other.title and self.done == other.done
+
+def test_todo(): # TEST CASES
+    todo1 = Todo('Buy milk')
+    todo2 = Todo('Clean room')
+    todo3 = Todo('Go to gym')
+    todo4 = Todo('Clean room')
+
+    print(todo1)                  # [ ] Buy milk
+    print(todo2)                  # [ ] Clean room
+    print(todo3)                  # [ ] Go to gym
+    print(todo4)                  # [ ] Clean room
+
+    print(todo2 == todo4)         # True
+    print(todo1 == todo2)         # False
+    print(todo4.done)             # False
+
+    todo1.done = True
+    todo4.done = True
+    print(todo4.done)             # True
+
+    print(todo1)                  # [X] Buy milk
+    print(todo2)                  # [ ] Clean room
+    print(todo3)                  # [ ] Go to gym
+    print(todo4)                  # [X] Clean room
+
+    print(todo2 == todo4)         # False
+
+    todo4.done = False
+    print(todo4.done)             # False
+    print(todo4)                  # [ ] Clean room
+
+test_todo()
