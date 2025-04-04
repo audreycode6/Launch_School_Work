e484a546a961ad45e06a7d86cd6123d242fee088
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-04-04 11:39:45 -0700

    updated TodoList class to have a each and select function

--- PY130/lesson_1/todo_class.py
+++ PY130/lesson_1/todo_class.py
@@ -82,7 +82,21 @@ class TodoList:
     
     def remove_at(self, index):
         self._todos.pop(index)
-    
+
+    def each(self, callback):
+        for todo in self._todos:
+            callback(todo)
+
+    def select(self, callback):
+        new_list = TodoList(self.title) # create new list
+
+        def choose(todo):
+            if callback(todo): # perform selection
+                new_list.add(todo) 
+
+        self.each(choose)
+        return new_list
+
 
 empty_todo_list = TodoList('Nothing Doing')
 
@@ -100,34 +114,20 @@ def setup():
 
     return todo_list
 
-def step_10():
-    print('--------------------------------- Step 10')
+def step_12():
+    print('--------------------------------- Step 12')
     todo_list = setup()
 
-    print(todo_list)
+    def y_in_title(todo):
+        return 'y' in todo.title
+
+    print(todo_list.select(y_in_title))
     # ---- Today's Todos -----
     # [ ] Buy milk
+    # [ ] Go to gym
+
+    print(todo_list.select(lambda todo: todo.done))
+    # ---- Today's Todos -----
     # [X] Clean room
-    # [ ] Go to gym
 
-    todo_list.remove_at(1)
-    print(todo_list)
-    # ---- Today's Todos -----
-    # [ ] Buy milk
-    # [ ] Go to gym
-
-    todo_list.remove_at(1)
-    print(todo_list)
-    # ---- Today's Todos -----
-    # [ ] Buy milk
-
-    try:
-        todo_list.remove_at(1)
-    except IndexError:
-        print('Expected IndexError: Got it!')
-
-    todo_list.remove_at(0)
-    print(todo_list)
-    # ---- Today's Todos -----
-
-step_10()
+step_12()
