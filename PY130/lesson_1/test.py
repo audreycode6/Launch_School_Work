cb327847b32a59ed29354481068dd14bd661c9af
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-04-08 12:11:18 -0700

    finished lesson 1

--- PY130/lesson_1/test.py
+++ PY130/lesson_1/test.py
@@ -1,13 +1,4 @@
-def reduce(callback, iterable, start):
-    accum = start
-    for elem in iterable:
-        accum = callback(elem, accum)
+def foo(integer, callback):
+    return callback(integer)
 
-    return accum
-
-
-
-# numbers = [1, 2, 4, 8]
-numbers = [3, 7, 2, 9, 5]
-print(reduce(lambda number, accum: (number**2) + accum, numbers, 0))        
-
+print(foo(5, lambda x: x * x))
