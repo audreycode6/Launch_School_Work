8d6f440e26d4eb19c89bd8291ef0d0daff7b3f67
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-05-07 14:49:17 -0700

    practice file for using regex in python

---
+++ PY130/challenge_py/regex_practice.py
@@ -0,0 +1,19 @@
+import re
+def danish(string):
+    print(re.sub(r'\b(apple|blueberry|cherry)\b', 'danish', string, count=1))
+
+
+danish('An apple a day keeps the doctor away')
+# -> 'An danish a day keeps the doctor away'
+
+danish('My favorite is blueberry pie')
+# -> 'My favorite is danish pie'
+
+danish('The cherry of my eye')
+# -> 'The danish of my eye'
+
+danish('apple. cherry. blueberry.')
+# -> 'danish. cherry. blueberry.'
+
+danish('I love pineapple')
+# -> 'I love pineapple'
