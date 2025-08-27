6a1463fd2318993b8a30433642c30fb960556863
Audrey Theriault-Allaire <audrey2699@gmail.com>
2025-08-26 17:04:03 -0700

    current progress on todos.app: -update_completion_status, delete_todo, complete_all_todos, edit_list, delete_lst, update_list_title new view templates: edit_lst.html, list.html new utils.py file to store helper funcs

--- PY175/todo_starter/app.py
+++ PY175/todo_starter/app.py
@@ -10,15 +10,18 @@ from flask import (
     url_for,
     )
 
-from todos.utils import error_for_list_title, error_for_title_length
+from todos.utils import (
+    error_for_list_title,
+    error_for_title_length,
+    find_list_by_id,
+    find_todo_by_id,
+    mark_todos_complete
+    )
 
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 app.secret_key = "secret1"
-
-def find_list_by_id(list_id, lists):
-    return next((lst for lst in lists if lst['id'] == list_id), None)
 
 
 @app.before_request
@@ -92,6 +95,85 @@ def add_todo(list_id):
     session.modified = True
     return redirect(url_for("show_list", list_id=list_id))
 
+@app.route('/lists/<list_id>/todos/<todo_id>/toggle', methods=['POST'])
+def update_completion_status(list_id, todo_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound('List not found.')
+    
+    todo = find_todo_by_id(todo_id, lst['todos'])
+    if not todo:
+        raise NotFound('Todo not found.')
+    
+    new_status = not todo.get('completed')
+    todo['completed'] = new_status
+    session.modified = True
+
+    message = 'complete' if new_status else 'incomplete'
+    flash(f'Todo marked {message}', 'success')
+    return redirect(url_for("show_list", list_id=list_id))
+
+@app.route('/lists/<list_id>/todos/<todo_id>/delete', methods=['POST'])
+def delete_todo(list_id, todo_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst: 
+        raise NotFound("List not found.")
+    
+    todo = find_todo_by_id(todo_id, lst['todos'])
+    if not todo:
+        raise NotFound('Todo not found.')
+    
+    lst['todos'].remove(todo) # delete todo
+    session.modified = True
+    flash('Todo has been successfully deleted.', 'success')
+    return redirect(url_for('show_list', list_id=list_id))
+
+@app.route("/lists/<list_id>/complete_all", methods=['POST'])
+def complete_all_todos(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound("List not found.")
+    
+    mark_todos_complete(lst['todos'])
+    session.modified = True
+    flash('Todos have been marked complete!', 'success')
+    return redirect(url_for('show_list', list_id=list_id))
+
+@app.route("/lists/<list_id>/edit")
+def edit_list(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound("List not found.")
+    
+    return render_template("edit_list.html", lst=lst)
+
+@app.route("/lists/<list_id>/delete", methods=['POST'])
+def delete_list(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound("List not found.")
+
+    session['lists'].remove(lst)
+    session.modified = True
+    flash(f"List has been deleted.", "success")
+    return redirect(url_for("get_lists"))
+    
+@app.route("/lists/<list_id>", methods=['POST'])
+def update_list_title(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound("List not found.")
+    new_title = request.form['list_title'].strip()
+    
+    error = error_for_list_title(new_title, session['lists'])
+    if error:
+        flash(error, "error")
+        return render_template('edit_list.html', lst=find_list_by_id(list_id, session['lists']), title=new_title)
+    
+    lst['title'] = new_title
+    session.modified = True
+    flash("Successfully updated title.", "success")
+    return redirect(url_for("show_list", list_id=list_id))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- PY175/todo_starter/todos/utils.py
+++ PY175/todo_starter/todos/utils.py
@@ -11,3 +11,12 @@ def error_for_list_title(title, lists):
         return "The title must be unique."
     return error_for_title_length(title)
 
+def find_list_by_id(list_id, lists):
+    return next((lst for lst in lists if lst['id'] == list_id), None)
+
+def find_todo_by_id(todo_id, todos):
+    return next((todo for todo in todos if todo['id'] == todo_id), None)
+
+def mark_todos_complete(todos):
+    for todo in todos:
+        todo['completed'] = True
