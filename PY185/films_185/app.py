279ab22cbc25794c3b93d8d8f7eb5e25614e982f
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-02-13 11:47:03 -0800

    practice psycopg2 to interact with db, assignment 4 "Executing SQL Statements from Python"

--- PY185/films_185/app.py
+++ PY185/films_185/app.py
@@ -1,3 +1,28 @@
 import psycopg2
+from psycopg2 import extras
 
-print("Hello, World")
+connection = psycopg2.connect(dbname='films')
+
+try:
+    with connection:
+        with connection.cursor(cursor_factory=extras.DictCursor) as cursor:
+            cursor.execute(
+                """SELECT genre, count(id) FROM films
+                    WHERE duration < 110
+                    GROUP BY genre;"""
+                    )
+            genre_dict = cursor.fetchall() # gets remaining 
+
+finally:
+    connection.close()
+
+print(f'Connection closed: {connection.closed}')
+
+
+print(genre_dict)
+
+for genre, films in genre_dict:
+    print(genre, films)
+
+for row in genre_dict:
+    print(f'Genre: {row['genre']}, Films: {row['count']}')
