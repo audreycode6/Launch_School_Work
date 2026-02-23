6448d42823cfe3099805dce7f1264db0abd30852
Audrey Theriault-Allaire <audrey2699@gmail.com>
2026-02-23 10:17:38 -0800

    add functionality for loading the todos from the db

--- todos_app/todos/database_persistence.py
+++ todos_app/todos/database_persistence.py
@@ -21,6 +21,14 @@ class DatabasePersistence:
         finally:
             connection.close()
 
+    def _find_todos_for_list(self, list_id):
+        query = "SELECT * FROM todos WHERE list_id = %s;"
+        logger.info("Executing query: %s with list_id: %s", query, list_id)
+        with self._database_connect() as conn:
+            with conn.cursor(cursor_factory=DictCursor) as cursor:
+                cursor.execute(query, (list_id,))
+                return cursor.fetchall()
+
     def all_lists(self):
         query = "SELECT * FROM lists;"
         logger.info("Executing query: %s", query)
@@ -31,7 +39,9 @@ class DatabasePersistence:
             
         lists = [dict(result) for result in results]
         for lst in lists:
-            lst.setdefault('todos', [])
+            todos = self._find_todos_for_list(lst['id'])
+            lst.setdefault('todos', todos)
+
         return lists
 
     def find_list(self, list_id):
@@ -42,7 +52,8 @@ class DatabasePersistence:
                 cursor.execute(query, (list_id,))
                 lst = dict(cursor.fetchone())
 
-        lst.setdefault('todos', [])
+        todos = self._find_todos_for_list(list_id)
+        lst.setdefault('todos', todos)
         return lst
     
     def create_new_list(self, title):
